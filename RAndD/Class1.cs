7df9f9af65cac690e476c2cb0bdc3dbfd1264e1a
Tejaswi Katta <tejaswikat@valuemomentum.biz>
2020-02-03 17:54:15 +0530

    ADDING VS code

---
+++ RAndD/Class1.cs
@@ -0,0 +1,25 @@
+﻿namespace RAndD
+{
+    public class Class1
+    {
+        System.Boolean
+            bool
+        public int ID
+        {
+            get => default(int);
+            set
+            {
+            }
+        }
+
+        public string Name
+        {
+            get => default(string);
+            set
+            {
+            }
+            
+          
+        }
+    }
+}
